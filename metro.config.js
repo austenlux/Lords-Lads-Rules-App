a1c1c145d67040717852fc6ffc045954ed19758f
Austen Lux <austenlux@gmail.com>
2025-03-14 14:03:41 -0400

    chore: rename app to Lords and Lads Rules

--- metro.config.js
+++ metro.config.js
@@ -1,22 +1,6 @@
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require('@react-native/metro-config');
 
-const defaultConfig = getDefaultConfig(__dirname);
-
-module.exports = {
-  ...defaultConfig,
-  resolver: {
-    ...defaultConfig.resolver,
-    sourceExts: ['jsx', 'js', 'ts', 'tsx', 'json']
-  },
-  watchFolders: [__dirname],
-  server: {
-    ...defaultConfig.server,
-    enhanceMiddleware: (middleware) => {
-      return (req, res, next) => {
-        // Increase the max HTTP header size
-        req.maxHeaderBytes = 64000;
-        middleware(req, res, next);
-      };
-    }
-  }
-}; 
+module.exports = (async () => {
+  const config = await getDefaultConfig(__dirname);
+  return config;
+})(); 
