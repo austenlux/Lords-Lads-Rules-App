1bb5ad38b47b298c452f442052deff377d1da87d
Austen Lux <austenlux@gmail.com>
2025-03-18 14:31:16 -0400

    Improve arrow rotation in changelog expandable sections

--- App.js
+++ App.js
@@ -938,7 +938,7 @@ const InfoSettingsScreen = ({ lastFetchDate }) => {
                       transform: [{
                         rotate: animations['v1.2.0'].rotation.interpolate({
                           inputRange: [0, 1],
-                          outputRange: ['0deg', '90deg']
+                          outputRange: ['0deg', '180deg']
                         })
                       }]
                     }
@@ -984,7 +984,7 @@ const InfoSettingsScreen = ({ lastFetchDate }) => {
                       transform: [{
                         rotate: animations['v1.1.0'].rotation.interpolate({
                           inputRange: [0, 1],
-                          outputRange: ['0deg', '90deg']
+                          outputRange: ['0deg', '180deg']
                         })
                       }]
                     }
@@ -1036,7 +1036,7 @@ const InfoSettingsScreen = ({ lastFetchDate }) => {
                       transform: [{
                         rotate: animations['v1.0.0'].rotation.interpolate({
                           inputRange: [0, 1],
-                          outputRange: ['0deg', '90deg']
+                          outputRange: ['0deg', '180deg']
                         })
                       }]
                     }
@@ -2031,8 +2031,12 @@ const styles = StyleSheet.create({
     marginRight: 12,
   },
   versionArrow: {
-    fontSize: 16,
+    fontSize: 24,
     color: '#BB86FC',
+    width: 24,
+    height: 24,
+    textAlign: 'center',
+    lineHeight: 24,
   },
   versionContentContainer: {
     overflow: 'hidden',
