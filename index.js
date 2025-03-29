f76d52160baff00fd687536c300be8636136c18c
Austen Lux <austenlux@gmail.com>
2025-03-29 09:39:17 -0400

    Refactor iOS project structure and update app registration
    - Changed app registration name from `appName` to a hardcoded 'main' in index.js.
    - Updated package name from `lords-and-lads-rules` to `lords-and-lads` and version to `1.2.2` in package.json and package-lock.json.
    - Removed Expo-related files and references from the iOS project, including Expo.plist and ExpoModulesProvider.
    - Adjusted Podfile to streamline dependencies and configurations.
    - Enhanced AppDelegate to use React's RCTAppDelegate instead of Expo's wrapper.
    - Added support for Hermes and updated build settings for better compatibility.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
-import { name as appName } from './app.json';
 
-// Register the app with the exact same name as in MainActivity.java
-AppRegistry.registerComponent(appName, () => App);
+// Register the app with 'main' as the component name
+AppRegistry.registerComponent('main', () => App);
