d73cb336112c110610683ea6b765adb49996c761
Austen Lux <austenlux@gmail.com>
2025-04-04 08:53:33 -0400

    Refactor iOS project structure and update configurations
    - Removed Expo-related files including app.json and eas.json.
    - Updated Podfile to target iOS 15.1 and adjusted project settings.
    - Enhanced .gitignore to exclude build artifacts and generated files.
    - Changed app registration name in index.js to 'LordsandLadsRules'.
    - Updated package dependencies in package-lock.json.
    - Removed unnecessary files and streamlined project structure for better maintainability.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 
-// Register the app with 'main' as the component name
-AppRegistry.registerComponent('main', () => App);
+// Register the app
+AppRegistry.registerComponent('LordsandLadsRules', () => App);
